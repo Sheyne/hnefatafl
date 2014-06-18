5ff032af2086e854c96c5208f58cfd0999567922
Joseph Colicchio <jcolicchio@novacoast.com>
2014-06-17 23:50:03 -0700

    Modified stuff

--- logic.js
+++ logic.js
@@ -225,26 +225,4 @@ exports.makeMove = function (player, selectedX, selectedY, x, y) {
 
 
 
-initUI();
-updateUI();
 
-boardElement.on('click', '.board-cell', function () {
-    var cell = $(this);
-    var x = cell.data('x');
-    var y = cell.data('y');
-    if (!exports.makeMove(gameState.turn, gameState.selectedPiece.x, gameState.selectedPiece.y, x, y)) {
-        var piece = gameState.board[y][x];
-        if (PieceTeams[piece] == gameState.turn && (x != gameState.selectedPiece.x || y != gameState.selectedPiece.y)) {
-            gameState.selectedPiece = {
-                "x": x,
-                    "y": y
-            };
-        } else {
-            gameState.selectedPiece = {
-                "x": -1,
-                    "y": -1
-            };
-        }
-    }
-    updateUI();
-});
